b813aaea5bbb86041f395079cbda6431bd6c7857
ManuAviles <mam00@iesemilidarder.com>
2018-10-29 17:57:01 +0100

    Continuando..

--- src/main/java/com/iesemilidarder/maviles/Launcher.java
+++ src/main/java/com/iesemilidarder/maviles/Launcher.java
@@ -1,4 +1,19 @@
 package com.iesemilidarder.maviles;
 
-public class Launcher {
+import com.iesemilidarder.maviles.data.Activities;
+import com.iesemilidarder.maviles.data.Countries;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Launcher  {
+
+    private static Object Countries;
+
+    public static void main(String... args) {
+
+    Activities activities = new Activities();
+
+    }
 }
--- src/main/java/com/iesemilidarder/maviles/data/Products.java
+++ src/main/java/com/iesemilidarder/maviles/data/Products.java
@@ -2,31 +2,8 @@ package com.iesemilidarder.maviles.data;
 
 public abstract class Products {
 
-private String country;
-private Double Price;
-private Integer npersons;
+protected String country;
+protected Double Price;
+protected Integer npersons;
 
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public Double getPrice() {
-        return Price;
-    }
-
-    public void setPrice(Double price) {
-        Price = price;
-    }
-
-    public Integer getNpersons() {
-        return npersons;
-    }
-
-    public void setNpersons(Integer npersons) {
-        this.npersons = npersons;
-    }
 }
